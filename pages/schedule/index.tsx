9abb196d8af7baadbc56a8b943a6043da2a37814
Igor Gordeev <ihar.hardzeyeu@itechart-group.com>
2023-02-28 19:41:41 +0300

    feat: add loader to table

--- pages/schedule/index.tsx
+++ pages/schedule/index.tsx
@@ -3,20 +3,26 @@ import useSwr from 'swr';
 import { useRouter } from 'next/router';
 import { ISchedule } from '@/types/types';
 import ScheduleTable from '@/components/ScheduleTable';
+import { CircularProgress } from '@mui/material';
+import styles from '@/styles/Home.module.css';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Schedule = () => {
     const router = useRouter();
     const params = router.query;
-    const { data } = useSwr<ISchedule>(
+    const { data, isLoading } = useSwr<ISchedule>(
         `/api/schedule?from=${params.from}&to=${params.to}`,
         fetcher
     );
 
     return (
-        <div>
-            <ScheduleTable connections={(data && data?.connections) || []} />
+        <div className={styles.main}>
+            {isLoading ? (
+                <CircularProgress />
+            ) : (
+                <ScheduleTable connections={(data && data?.connections) || []} />
+            )}
         </div>
     );
 };
